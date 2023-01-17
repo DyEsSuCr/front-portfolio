fed8f57cd12f61265f9469072175266662084d8e
DylanSuarez <2000sajo@gmail.com>
2023-01-17 01:46:59 -0500

    skills object

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <About />
-      {/* <Skills /> */}
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,18 +1,107 @@
+// Skills
+import CSS from './../images/skills/css-logo.webp';
+import Django from './../images/skills/django-logo.png';
+import Figma from './../images/skills/figma-logo.png';
+import Git from './../images/skills/git.png';
+import HTML from './../images/skills/html-logo.png';
+import JS from './../images/skills/js-logo.png';
+import MySQL from './../images/skills/mysql-logo.png';
+import Npm from './../images/skills/npm-logo.png';
+import PostgreSQL from './../images/skills/postgresql-logo.png';
+import React from './../images/skills/react-logo.png';
+import RestFramework from './../images/skills/restframework-logo.webp';
+import Sass from './../images/skills/sass-logo.png';
+import Tailwind from './../images/skills/tailwind-logo.png';
+
 import { useState, useEffect } from 'react';
 
+const api_skills = [
+  {
+    id: crypto.randomUUID(),
+    name: 'Django',
+    logo: Django,
+    url: 'https://www.djangoproject.com',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Rest Framework',
+    logo: RestFramework,
+    url: '',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'JavaScript',
+    logo: JS,
+    url: 'https://developer.mozilla.org/es/docs/Web/JavaScript',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'React',
+    logo: React,
+    url: 'https://es.reactjs.org/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'MySQL',
+    logo: MySQL,
+    url: 'https://www.mysql.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'PostgreSQL',
+    logo: PostgreSQL,
+    url: 'https://www.postgresql.org/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Tailwind',
+    logo: Tailwind,
+    url: 'https://tailwindcss.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Sass',
+    logo: Sass,
+    url: 'https://sass-lang.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'CSS',
+    logo: CSS,
+    url: 'https://developer.mozilla.org/es/docs/Web/CSS',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Git',
+    logo: Git,
+    url: 'https://git-scm.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'HTML',
+    logo: HTML,
+    url: 'https://developer.mozilla.org/es/docs/Web/HTML',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'NPM',
+    logo: Npm,
+    url: 'https://www.npmjs.com/',
+  },
+
+  {
+    id: crypto.randomUUID(),
+    name: 'Figma',
+    logo: Figma,
+    url: 'https://www.figma.com/',
+  },
+];
+
 const Skills = () => {
-  const url = 'https://api-portfolio-hulf.onrender.com/skills/';
   const [skills, setSkills] = useState();
 
-  const fetchApi = async () => {
-    const response = await fetch(url);
-    const responseJSON = await response.json();
-
-    setSkills(responseJSON);
-  };
-
   useEffect(() => {
-    fetchApi();
+    setSkills(api_skills);
   }, []);
 
   return (
